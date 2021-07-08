09d4aefd720ec81fc39f534243f76112a1468967
Sergey Podguzov <spo@virtoway.com>
2021-07-08 18:19:37 +0500

    VME-1176: Namespace for StringBuilder after refactor Startup.cs

--- VirtoCommerce.Storefront/Startup.cs
+++ VirtoCommerce.Storefront/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Text;
 using System.Text.Encodings.Web;
 using System.Text.Unicode;
 using FluentValidation.AspNetCore;
